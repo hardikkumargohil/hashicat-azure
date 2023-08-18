865b3570d1a4eb8a7ba7fc9306d91e5a2366c652
Hardikkumar Gohil <devops.poc101@gmail.com>
2023-08-18 01:18:36 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -2,5 +2,5 @@ module "network" {
   source  = "app.terraform.io/Terraform-Azure-Workshop-20230426/network/azurerm"
   version = "3.5.0"
   # insert required variables here
-  resource_group_name = "GroupName"
+  resource_group_name = "network"
 }
